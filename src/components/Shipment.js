0e3adf2b39872be67db726edb73abfe67ea3893e
Maksym <sat2151@gmail.com>
2021-07-17 13:51:38 +0300

    Finished component Order

---
+++ src/components/Shipment.js
@@ -0,0 +1,30 @@
+﻿import React,{Component} from 'react'
+
+export default class Shipment extends Component{
+    render(){
+
+        const { total } = this.props;
+        const shipping = total > 0 && total < 500 ? 350 :99;
+        const shippingNeon= shipping === 99 ?(
+            <span className='font-effect-neon total_wrap-cheap'>
+                {shipping}₴
+            </span>
+        ) : (
+            <span> {shipping} ₴</span>
+        )
+        return(
+            <div className="total">
+                <div className="total_wrap">
+                    <div>
+                        <div>Доставка: {total > 0 ? shippingNeon : null}</div>
+                        <div className="total_wrap-free">
+                            {total < 500 ? `Закажите еще на ${500-total} ₴ для доставки за 99 ₴`: null}
+                        </div>
+                    </div>
+
+                     <div className="total_wrap-final">Итог: {total} ₴</div>
+                 </div>
+            </div>
+        )
+    }
+}
